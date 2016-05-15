60c378034b958bb8d8ed2af68d14e748869aff3f
francisco <franciscoramonsp@outlook.com>
2016-05-15 19:05:07 -0300

    adição de chagem de tipo

--- contato.py
+++ contato.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 class Contato(object):
     contatos = []
     def add_contato(self,celular):
+        if isinstance(celular,type(self)) == False:
+            raise TypeError("o remetente não é um object Celular")
         self.contatos.append(celular)
     def lista_contatos(self):
         for c in self.contatos:
